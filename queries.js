acd5efea759a0d776ed99df77dcfd1228955a012
Mike Wekesa <mikewamalwa92@gmail.com>
2025-09-27 11:40:48 +0300

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,1 +1,18 @@
+// Find all books in a specific genre
+db.books.find({ genre: "Programming" });
+
+// Find books published after 2015
+db.books.find({ published_year: { $gt: 2015 } });
+
+// Find books by a specific author
+db.books.find({ author: "Robert C. Martin" });
+
+// Update price of a specific book
+db.books.updateOne(
+  { title: "Clean Code" },
+  { $set: { price: 35 } }
+);
+
+// Delete a book by its title
+db.books.deleteOne({ title: "The Pragmatic Programmer" });
 
