48738fa987bc3b7001fb05a18222ba1805e7abcc
Mike Wekesa <mikewamalwa92@gmail.com>
2025-09-27 11:38:38 +0300

    Update insert_books.js

--- insert_books.js
+++ insert_books.js
@@ -1,1 +1,24 @@
+db.books.insertMany([
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    genre: "Programming",
+    published_year: 2008,
+    price: 30,
+    in_stock: true,
+    pages: 464,
+    publisher: "Prentice Hall"
+  },
+  {
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
+    genre: "Programming",
+    published_year: 1999,
+    price: 25,
+    in_stock: true,
+    pages: 352,
+    publisher: "Addison-Wesley"
+  },
+  // ➕ Add at least 8 more book docs
+]);
 
